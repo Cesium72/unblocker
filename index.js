92ed0dc7058bb2944667746b29fc85ac0abbaa89
Cesium72 <bmctilton@gmail.com>
2024-03-15 16:59:48 -0600

    h

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-// run `node index.js` in the terminal
+const http = require('http');
+const types = {
+    "html":"text/html",
+    "txt":"text/plain",
+    "text":"text/plain",
+    "css":"text/css",
+    "js":"text/javascript",
+    "mjs":"text/javascript",
+    "png":"image/png",
+    "ico":"image/ico",
+    "jpg":"image/jpg",
+    "jpeg":"image/jpeg"
+};
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+function serve(req,res) {
+    var url = req.url.slice(1);
+    try {
+    console.log("1")
+    res.setHeader("Content-type",types[url.split(".")[url.split(".").length-1]]);
+    console.log("2")
+    child_process.exec('curl https://hobbyrobot.com/' + url, (err, stdout) => {res.write(stdout);console.log(stdout)})
+    } catch {
+        res.setHeader("Content-type","text/html");
+        res.write("Something went wrong!")
+        console.log("err")
+    }
+    //res.write("Hello!");
+    res.end();
+}
+
+const server = http.createServer(serve);
+server.listen(2048);
